d7d3e4293bd798cba167cf27cebf773e2368e90b
wander-luiz <wander-luiz@test.com>
2022-04-27 14:19:26 -0300

    Adicao da classe TipoCozinha e criacao do TipoCozinhaRepository

---
+++ src/main/java/br/com/felipe/messias/hasfood/repository/TipoCozinhaRepository.java
@@ -0,0 +1,9 @@
+package br.com.felipe.messias.hasfood.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.felipe.messias.hasfood.model.TipoCozinha;
+
+public interface TipoCozinhaRepository extends JpaRepository<TipoCozinha, Integer> {
+
+}
